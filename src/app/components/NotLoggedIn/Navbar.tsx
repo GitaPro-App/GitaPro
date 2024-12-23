2b6a7e32cdf4e6ef97de193dd5b2a1cb4a9d5e5e
sanjeet verma <sxv210116@utdallas.edu>
2024-12-23 16:31:31 -0600

    Changes

--- src/app/components/NotLoggedIn/Navbar.tsx
+++ src/app/components/NotLoggedIn/Navbar.tsx
@@ -25,13 +25,13 @@ export default function Navbar() {
         <div className="container mx-auto px-4 max-w-6xl">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
-            <a href = '/'>
+            <Link href = '/'>
               <button>
                 <div className="flex-shrink-0 px-1">
                   <h1 className="text-3xl font-bold text-white">GitaPro</h1>
                 </div>  
               </button>
-            </a>
+            </Link>
 
             
 
